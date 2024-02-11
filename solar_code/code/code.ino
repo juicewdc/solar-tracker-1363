2b886e5038a9199e17ef4c08f406697891de4def
juicewdc <129550862+juicewdc@users.noreply.github.com>
2024-02-11 23:02:59 +0300

    newcode

---
+++ solar_code/code/code.ino
@@ -0,0 +1,67 @@
+#include <Servo.h>
+
+#define LU A0
+#define LD A1
+#define RU A2
+#define RD A3
+#define error 200
+
+int Spoint180 = 10;
+int Spoint360 = 50;
+Servo servo180;
+Servo servo360;
+
+void setup() {
+  Serial.begin(9600);
+  servo180.attach(5);
+  servo360.attach(6);
+}
+
+void loop() {
+  servo180.write(Spoint180);
+  servo360.write(Spoint360);
+  int lu = analogRead(LU);
+  int ld = analogRead(LD);
+  int ru = analogRead(RU);
+  int rd = analogRead(RD);
+
+  int d = (rd + ld) / 2;
+  int r = (ru + rd) / 2;
+  int u = (ru + lu) / 2;
+  int l = (ld + lu) / 2;
+
+  Serial.print(l);
+  Serial.print(", ");
+  Serial.print(r);
+  Serial.print(", ");
+  Serial.print(u);
+  Serial.print(", ");
+  Serial.println(d);
+
+
+  if ((lu <= error) && (ld <= error) && (ru <= error) && (rd <= error)) {
+    // Do nothing
+  } else {
+    if (d > 10) {
+    servo180.write(90)  ;
+    }
+    if (u > 10) {
+      servo180.write(180);
+    }
+  }
+
+  if ((lu <= error) && (ld <= error) && (ru <= error) && (rd <= error)) {
+    // Do nothing
+  } else {
+    if (r > 100) {
+      servo360.write(0);
+    }
+    if (l > 100) {
+      servo360.write(180);
+    } else {
+      servo360.write(90);
+    }
+  }
+
+  delay(100);
+}
